d99fad03cb9d0ef59ce96e0d398566075007542a
GabrielBladh <gabrielbladh@hotmail.com>
2025-12-15 12:52:17 +0100

    spelplan

--- spelplan.java
+++ spelplan.java
@@ -1,33 +1,43 @@
 public class spelplan
 {
     public static AutoTester autoTester;
-
     public static void main (String[] args)
     {
         autoTester = new AutoTester();
         int gameField[][] = {{1, 1, 2, 3, 3},{2, 1, 1, 2, 3},{3, 2, 2, 1, 2},{3, 3, 3, 3, 3}};
         int position = 0;
-        int dödligPosition = 0;
-        String expectedResults[][] = {};
+        int sumDödligPosition = 0;
+        char expectedResults[][] = new char[gameField.length][gameField[0].length];
         for (int row = 0; row < gameField.length; row++)
         {
             for (int col = 0; col < gameField[row].length; col++)
             {
-                System.out.println(gameField[row][col]);  
-                position = gameField[row][col];
-                for (int row1 = -1; row1 < 2; row1++)
+                sumDödligPosition = 0;
+                for (int row1 = row -1; row1 <= row + 1; row1++)
                 {
-                    for (int col1 = -1; col1 < 2; col1++)
+                    for (int col1 = col -1; col1 <= col + 1; col1++)
                     {
-                        dödligPosition = 
-                    }
+                        if (row1 >= 0 && row1 < gameField.length && col1 >= 0 && col1 < gameField[row].length)
+                        {
+                            sumDödligPosition += gameField[row1][col1];
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }   
                 }
-                if (gameField[row][col] == 3)
+                sumDödligPosition -= gameField[row][col];
+                if (gameField[row][col] == 3 || sumDödligPosition >= 15)
                 {
-
+                    expectedResults[row][col] = 'F';
+                }
+                else
+                {
+                    expectedResults[row][col] = 'T';
                 }
             }
-            System.out.println();
         }
+        autoTester.testA9(gameField, expectedResults);
     }
 }
